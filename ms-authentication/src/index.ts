13cf85521a61c0e5df8d543fb7994a2cafb4744e
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-01-07 11:45:42 -0300

    Configuração das rotas POST-PUT-DELETE

--- ms-authentication/src/index.ts
+++ ms-authentication/src/index.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response, NextFunction } from "express"
+import express from "express"
 import userRouter from "./routes/user.routes"
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.use(userRouter)
 
-app.listen(5000, ()=> {
-    console.log("server ok 5000")
+app.listen(4000, ()=> {
+    console.log("server ok 4000")
 })
--- ms-authentication/src/routes/user.routes.ts
+++ ms-authentication/src/routes/user.routes.ts
@@ -1,17 +1,40 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+
 
 const userRouter =  Router()
-const users = [{
-    name: "Gustavo"
-}]
+
 
 userRouter.get("/users", (req: Request, res: Response, next: NextFunction) => {
+    const users = [{name: "Gustavo"}]
     res.send(users)
 })
 
 userRouter.get("/users/:id", (req: Request<{id:string}>, res: Response, next: NextFunction) => {
     const id = req.params.id
-    res.send(users)
+    res.send(id)
+})
+
+userRouter.post("/users", (req: Request, res: Response, next: NextFunction) => {
+    const {name} = req.body
+    res.send(name)
+
+})
+
+userRouter.put("/users/:id", (req: Request<{id:string}>, res: Response, next: NextFunction) => {
+   
+    const id = req.params.id
+    const modifiedUser = req.body
+
+    modifiedUser.id = id
+
+    res.send(modifiedUser)
+
+})
+
+
+userRouter.delete("/users/:id", (req: Request<{id:string}>, res: Response, next: NextFunction) => {
+    const id = req.params.id
+    res.send(id)
 })
 
 export default userRouter
