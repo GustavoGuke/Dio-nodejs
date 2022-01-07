9d468d44acf8c819720c1b6a309a2c40e1b5be99
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-01-06 23:23:34 -0300

    Biblioteca ts-node-dev para automatizar server

--- ms-authentication/src/index.ts
+++ ms-authentication/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello word typescript")
+import express, { Request, Response, NextFunction } from "express"
+
+const app = express()
+app.use(express.json())
+
+app.get("/status", (req: Request, res: Response, next: NextFunction)=> {
+    res.send("servidor ok")
+})
+
+app.listen(5000, ()=> {
+    console.log("server ok 5000")
+})
